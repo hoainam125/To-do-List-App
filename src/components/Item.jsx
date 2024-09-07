29f211742f26b1160b4dbdeaa07b700c7e10fa0e
NGUYỄN HOÀI NAM <nguyenhnam21@gmail.com>
2024-09-07 21:26:57 +0700

    feat: input box

---
+++ src/components/Item.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import tick from '../assets/tick.png'
+const Item = () => {
+  return (
+    <div className='flex items-center my-3 gap-2'>
+      <div>
+        <img src="{tick" alt="" />
+      </div>
+    </div>
+  )
+}
+ className='flex items-center my-3 gap-2'
+export default Item
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import todo from '../assets/todo.png'
 const Todo = () => {
   return (
-    <div className=''>
-      <h1>Hello World</h1>
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-x1'>
+    {/* Title */}
+      <div className='flex items-center pt-7 gap-2'>
+        <img src={todo} alt='todo' className='w-8 h-8' />
+        <h1 className='text-3xl font-semibold'> To-Do List</h1>
+      </div>
+      {/* input box */}
+
+      <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+        <input type="text" placeholder='Add your task' className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' />
+        <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+      </div>
+      {/* Todo */}
+      <div>
+      
+
+      </div>
     </div>
   )
 }
